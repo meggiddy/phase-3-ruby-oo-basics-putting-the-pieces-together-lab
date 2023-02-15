4ce409083d0c2d38d917aa11d805efb2a3ccc405
meggidy <mimamitchelle@gmail.com>
2023-02-15 11:51:48 +0300

    Completed

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,15 @@
+class Book
+  attr_accessor :title, :author_name, :page_count, :genre
 
+  def initialize(title, author_name, page_count, genre)
+    @title = title
+    @author_name = author_name
+    @page_count = page_count
+    @genre = genre
+  end
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
 
+Book_2 = Book.new("And Then There Were None", "Agatha Christie", 272, "Mystery")
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,18 @@
 # Make your shoe class here!
+class Shoe
+  attr_accessor :brand, :color, :size, :material, :condition
+  attr_reader :brand
+  def initialize(brand, color, size, material, condition)
+    @brand = brand
+    @color = color
+    @size = size
+    @material = material
+    @condition = condition
+  end
+  def cobble
+    puts "Your shoe is good as new!"
+    @condition = "new"
+  end
+end
+
+Shoe_2 = Shoe.new("Nike", "red", 9.5, "suede", "tattered")
